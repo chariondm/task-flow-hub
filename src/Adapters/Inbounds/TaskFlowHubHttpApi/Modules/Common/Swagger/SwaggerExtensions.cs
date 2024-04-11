2214b5142d82289fdc7d20960f8815666b4fabca
Marcelo Souza <chariondm@gmail.com>
2024-04-11 16:35:21 -0300

    feat(swagger): add bearer token authentication for API docs
    Implemented Bearer Token authentication support in the Swagger configuration to enable API testing with JWT authentication through the Swagger UI. This enhancement allows users to authenticate directly via Swagger by inserting a JWT token, facilitating a more secure and user-friendly testing environment.

--- src/Adapters/Inbounds/TaskFlowHubHttpApi/Modules/Common/Swagger/SwaggerExtensions.cs
+++ src/Adapters/Inbounds/TaskFlowHubHttpApi/Modules/Common/Swagger/SwaggerExtensions.cs
@@ -33,6 +33,36 @@ public static class SwaggerExtensions
                 }
             });
 
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
             var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
         });
